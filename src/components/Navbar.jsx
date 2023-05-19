aefbf1db683a5041d6a49349da9f42d174b690f7
Scarlet Rosa <likedyourbody@gmail.com>
2023-05-19 15:23:22 +0200

    3d pc appears at the screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/" className='flex items-center gap-2'
@@ -19,9 +19,9 @@ const Navbar = () => {
 
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className='text-white text-[18px] cursor-pointer flex'>Scarlet&nbsp;Rosa&nbsp;
-            <span className='sm:block hidden'>|&nbsp;Full&nbsp;Stack&nbsp;Developer</span></p>
+            <span className='sm:block hidden'>|&nbsp;Web&nbsp;Developer</span></p>
         </Link>
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
+        <ul className='list-none hidden sm:flex flex-row gap-6'>
           {navLinks.map((Link) => (
             <li key={Link.id} className={`${active === Link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(Link.title)}>
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+            <ul className='list-none flex justify-end items-start flex-col gap-2'>
               {navLinks.map((Link) => (
                 <li key={Link.id} className={`${active === Link.title ? "text-white" : "text-secondary"}
                 font-poppins font-medium cursor-pointer text-[16px]`}
