e65402c9834158df516dfb72fd9d89d0008310ff
Scarlet Rosa <likedyourbody@gmail.com>
2023-05-25 00:44:10 +0200

    modify colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[70px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-gradient-to-t from-purple-400 to-pink-600" />
+          <div className="w-1 sm:h-80 h-40 bg-gradient-to-r from-purple-100 to-pink-400" />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Sky
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, you can call me <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-200 via-pink-600 to-pink-200">Sky ㋛
           </span></h1>
           <h2 className={`${styles.heroSubText} mt-2 text-white-100 flex`}>
             I'm a Junior <p className="sm:block hidden">&nbsp;FullStack Web&nbsp;</p> Developer
