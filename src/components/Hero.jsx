f0d6c8489270d6e45ec89975fa414151fb50b6bf
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-08-12 15:01:26 -0400

    updating me

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { me } from "../assets";
 
 const Hero = () => {
   return (
