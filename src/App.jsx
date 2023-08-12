67bd1c3bf97538e08779a71a69307c61f91b669f
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-08-12 14:10:51 -0400

    updating works and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
-const App = () => {
+import { BrowserRouter } from "react-router-dom";
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -9,17 +10,18 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
